ac94b11ae820a9040758d0f92532d6a987147a29
hu-maeruf <179197304+hu-maeruf@users.noreply.github.com>
2025-12-17 15:31:32 +0300

    Add an operate function
    Takes three arguments, namely operands and operator(+, -, *, /)
    Call the respective function based on the operator

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return Math.round((num1 / num2) * 100) / 100;
 }
+
+
+function operate(num1, sign, num2) {
+    switch (sign) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+        default:
+            alert("Not the correct operation!")
+            break;
+    };
+};
